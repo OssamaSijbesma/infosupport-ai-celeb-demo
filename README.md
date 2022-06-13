9bce8c8ba480374036ea364e6b7da69a5448c4b3
Ossama <ossama_sybesma@hotmail.com>
2022-06-13 23:46:08 +0200

    feat: Made a fast API variant

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 Prerequirements
 
-- Python 3.9
+- Python 3.10.5
 
-Run the following commands:
+Navigate to the project folder and run the following commands:
 
 ```
+  python -m venv ./venv
   pip install -r requirements.txt
   $env:FLASK_APP="app"
   $env:FLASK_ENV = "development"
 ```
 
 Replace the following files:
+
+- https://github.com/rcmalli/keras-vggface/pull/74/files
 
 - `[venv]/google_images_download/google_images_download.py` with `lib-fixes/google_images_download.py`.
 - `[venv]/keras_vggface/utils.py` with `lib-fixes/utils.py`.
---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, File
+from predictor import predict_celeb
+from qwant import get_celeb_image
+import numpy as np
+import cv2
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"Status": "Working"}
+
+
+@app.post("/predict")
+async def predict(file: bytes = File()):
+    data = []
+    # request_object_content = await file.read()
+    image_raw = np.asarray(bytearray(file), dtype='uint8')
+    image = cv2.imdecode(image_raw, cv2.IMREAD_COLOR)
+    results = predict_celeb(image)[0]
+    print(results)
+
+    for name, confidence in results[0:3]:
+        name = name.split(" ")[-1].strip("'").replace('_', ' ')
+        # name = name.encode('unicode_escape').decode('utf-16')
+        print(name)
+        celeb_image = get_celeb_image(name)
+        data.append(
+            {
+                'name': name,
+                'confidence': round(confidence*100, 1),
+                'image': celeb_image
+            }
+        )
+    return data
